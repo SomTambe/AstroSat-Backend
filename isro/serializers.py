66d4d9160341c8d90203ba80525e81ad51aea754
SomTambe <tambesom@gmail.com>
2021-03-23 16:46:38 +0530

    Add documentation for code.

--- isro/serializers.py
+++ isro/serializers.py
@@ -3,6 +3,16 @@ from .models import source
 from pub.serializers import *
 from pub.models import *
 
+"""
+class SourceSerializer(serializers.ModelSerializer)
+
+Arguments:
+    Input:
+        Django object of the `source` model, not a QuerySet.
+
+    Output:
+        Python dictionary of with the fields as instructed.
+"""
 class SourceSerializer(serializers.ModelSerializer):
     pubs = PubSerializer(many=True, read_only=True)
 
@@ -11,6 +21,16 @@ class SourceSerializer(serializers.ModelSerializer):
         exclude = ('id')
         depth=1
 
+"""
+class GetSource(serializers.ModelSerializer)
+
+Arguments:
+    Input:
+        Django object of the `source` model, not a QuerySet.
+
+    Output:
+        Python dictionary of with the fields as instructed.
+"""
 class GetSource(serializers.ModelSerializer):
     class Meta:
         model = source
--- isro/utils.py
+++ isro/utils.py
@@ -6,7 +6,16 @@ from rest_framework.response import Response
 from rest_framework import permissions, status
 from pub.models import *
 from .serializers import *
+"""
+function make_card(request)
 
+Arguments:
+    Input:
+        request : Django request object.
+
+    Output:
+        src : a source object, which will then be serialized by the view I have created in views.py. If unsuccessful, it returns a 404_NOT_FOUND request.
+"""
 def make_card(request):
     # assuming request is a POST request.
     # fields = {'ra','dec'}
@@ -17,6 +26,16 @@ def make_card(request):
     src = get_object_or_404(source, ra=body['ra'], dec=body['dec'])
     return src
 
+"""
+function add_src(request)
+
+Arguments:
+    Input:
+        request : Django request object.
+
+    Output:
+        src : Response object, 201 if the object is successfully created, or else, 404 or 406.
+"""
 def add_src(request):
     # assuming request is a POST request.
     # fields = {'name','ra','dec','astrosat'}
@@ -35,6 +54,16 @@ def add_src(request):
     except:
         return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
 
+"""
+function add_pub(request)
+
+Arguments:
+    Input:
+        request : Django request object.
+
+    Output:
+        src : Response object, 201 if the object is successfully created, or else 404.
+"""
 def add_pub(request):
     # assuming request is a POST request.
     # fields = {'title','link','sources'}
@@ -53,6 +82,16 @@ def add_pub(request):
         pub.add_src(src)
     return Response(status=status.HTTP_201_CREATED)
 
+"""
+function send_data(request)
+
+Arguments:
+    Input:
+        request : Django request object.
+
+    Output:
+        src : Response object with the data of all sources of all the available fields in JSON format.
+"""
 def send_data(request):
     # assuming request is a GET request.
     # returning parameters for a source = [{'name','ra','dec','astrosat'}, ...]
